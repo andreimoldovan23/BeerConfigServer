fa9edde0514825ef4e822f6b881a4d4079cb98e2
andreimoldovan23 <moldovanandrei2301@gmail.com>
2021-07-22 14:46:37 +0300

    Added external cloud configuration

---
+++ src/main/java/sfmc/configserver/config/LocalDiscoveryConfig.java
@@ -0,0 +1,11 @@
+package sfmc.configserver.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("localdiscovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscoveryConfig {
+}
